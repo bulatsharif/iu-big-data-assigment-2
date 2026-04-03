09b463a84aef667bb159a5d07fe6cf2d9fb7fc5f
bulatsaripov <b.sharipov@innopolis.university>
2026-04-04 01:33:25 +0300

    feat: add basic mapper

--- app/mapreduce/mapper1.py
+++ app/mapreduce/mapper1.py
@@ -1,1 +1,31 @@
-print("this is mapper 1")
+import sys
+
+
+cur_word = None
+cur_doc_id = None
+cur_count = 0
+
+for l in sys.stdin:
+    l = l.strip()
+    if not l:
+        continue
+    word, doc_id, count = l.split("\t")
+    count = int(count)
+    if cur_word is None:
+        cur_word = word
+        cur_doc_id = doc_id
+        cur_count = count
+    else:
+        if cur_word == word:
+            cur_count += count
+        else:
+            if cur_word:
+                print(f"{cur_word}\t{cur_doc_id}\t{cur_count}")
+            cur_word = word
+            cur_doc_id = doc_id
+            cur_count = count
+    if cur_word:
+        print(f"{cur_word}\t{cur_doc_id}\t{cur_count}")
+
+if cur_word == word:
+    print(f"{cur_word}\t{cur_doc_id}\t{cur_count}")
