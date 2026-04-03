9cf4de35ca06ce76def988ff1106a29a18227c73
bulatsaripov <b.sharipov@innopolis.university>
2026-04-04 01:36:08 +0300

    feat: add create index bash script

--- app/create_index.sh
+++ app/create_index.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Create index using MapReduce pipelines"
+hdfs dfs -rm -r /index
+hdfs dfs -mkdir -p /index
+hdfs dfs -put data /index/data
+
+hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
+    -files mapreduce/mapper1.py,mapreduce/reducer1.py \
+    -mapper "mapreduce/mapper1.py" \
+    -reducer "mapreduce/reducer1.py" \
+    -input /index/data \
+    -output /index/output
